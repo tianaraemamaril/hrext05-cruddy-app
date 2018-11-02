22ad66025f560547aec65f75b2705535ed55da73
Tiana Rae Mamaril <tianaraetran@gmail.com>
2018-11-02 01:24:44 -0700

    FORGOT TO ADD AND COMMIT THIS WHOLE TIME.  :( Mostly done - still  need to change the reset/delete button to delete individual items, and resize the bears once deleted.

--- TODO.md
+++ TODO.md
@@ -1,13 +1,13 @@
 ### Cruddy App Todo
 
-#### Basic Improvments (requirements)
+#### Basic Improvements (requirements)
 
 - [ ] create indivdiual items
 - [ ] delete individual items
 - [ ] edit individual items
 
 ##### Notes
-- [ ] Keep DOM and localStorage matching 
+- [ ] Keep DOM and localStorage matching  // store objects 
 - [ ] Remember event Delegation when adding new items to .show-text
 - [ ] make sure we do not duplicate data
 - [ ] add different values to the item
@@ -33,3 +33,41 @@
 
 #### My Spin
 (to be filled out by you)
+- [ ] create bears
+- [ ] feed / steal food from bears
+- [ ] delete bears
+
+<!-- 
+PLAN
+
+Cruddy App
+
+BEAR GAME
+Create - bears to be fed food items 
+
+Pooh - Honey // button in shape of honey
+Paddinton - Marmalade
+Volibear - Teemo
+
+button: feed bear
+// timesEaten++
+// enlarge bearSize by 10%
+button: steal food from bear 
+// timesEaten--
+// decrease bearSize by 10%
+
+Obj : {
+  name: 'Winnie the Pooh',
+  food: 'honey',
+  timesEaten: 0,
+}
+
+button.click -> increment or decrement timesEaten based on food
+
+
+// max time to feed bear is 5, then they go into a food coma and you can no longer feed them
+// sleeping picture of the bear
+
+// reset / delete button deletes obj in local storagw and resets bears to 0 timesEaten
+
+-->
--- app.js
+++ app.js
@@ -2,17 +2,176 @@
 listen for click event (edit)
 update text in local storage (with key)
 update display with new text value
+*/
+
+// to delete from local storage : localStorage.removeItem(key);
+
+$(document).ready(function(){
+  console.log('before\n', window.localStorage);
+
+  ///////////////////////////////////////
+  // BEARS
+  ///////////////////////////////////////
+
+  var createBear = function (name, food) {
+
+    return {
+      name: name,
+      food: food,
+      timesEaten: 0
+    }
+
+  };
 
 
- */
 
+  var paddington = localStorage.getItem('Paddington');
+
+  if (!paddington) {
+    paddington = createBear('Paddington', 'marmalade');
+  } else {
+    paddington = JSON.parse(paddington);
+  }
+
+
+  var pooh = localStorage.getItem('Pooh');
+
+  if(!pooh) {
+    pooh = createBear('Pooh', 'honey');
+  } else {
+    pooh = JSON.parse(pooh);
+  }
+
+
+  var volibear = localStorage.getItem('Volibear');
+
+  if (!volibear) {
+    volibear = createBear('Volibear', 'Teemo');
+  } else {
+    volibear = JSON.parse(volibear);
+  }
+
+  
+  ///////////////////////////////////////
+  // FUNCTIONS
+  ///////////////////////////////////////
+
+  var MAX = 5;
+  var MIN = 0;
+
+  var feed = function(bear) {
+
+    if (bear.timesEaten < MAX) { 
+
+      bear.timesEaten++;
+      var h = 50 + (bear.timesEaten * 10);
+      var w = 50 + (bear.timesEaten * 10);
+      
+      // add bear.food image
+
+      $('#' + bear.name).css({height: h + '%', width: w + '%'});
+      localStorage.setItem(bear.name, JSON.stringify(bear)); 
+
+    } else if (bear.timesEaten === 5) {
+
+      $('#' + bear.name).css({transform: 'rotate(90deg)'}); // degrees are not relative
+      localStorage.setItem(bear.name, JSON.stringify(bear));
+      console.log('FOOD COMA!!')
+    }
+    // how to stop bears from shrinking after they fall??
+    // remove event listeners?
+
+    console.log(bear);
+  }
+
+
+
+  var steal = function(bear) {
+
+    if (bear.timesEaten > MIN) {
+
+      bear.timesEaten--;
+      var h = 100 - ((MAX - bear.timesEaten) * 10);
+      var w = 100 - ((MAX - bear.timesEaten) * 10);
+
+      $('#' + bear.name).css({height: h + '%', width: w + '%'});
+      localStorage.setItem(bear.name, JSON.stringify(bear));
+
+    } 
+
+    console.log(bear);
+  }
+
+
+
+  // remove item from app
+
+  ///////////////////////////////////////
+  // EVENT LISTENERS
+  ///////////////////////////////////////
+
+  // listen for click event (del)
+  $('#delete').on('click', function(){
+    // clears local storage of saved info
+    localStorage.clear();
+
+    paddington = createBear('Paddington', 'marmalade');
+    pooh = createBear('Pooh', 'honey');
+    volibear = createBear('Volibear', 'Teemo');
+
+    $('#Paddington').css({transform: 'rotate(0deg)'}); 
+    $('#Pooh').css({transform: 'rotate(0deg)'});
+    $('#Volibear').css({transform: 'rotate(0deg)'});
+
+    //function to return default obj. func getDefaultPooh
+    // bear -> deafult bear
+
+
+    // bear size needs to be reset as well
+    // createBear has them at 0
+    // timesEaten === 0 needs to have a 
+    
+  });
+
+  $('#feed-paddington').on('click', function(){
+    feed(paddington);
+  }); 
+
+  $('#feed-pooh').on('click', function(){
+    feed(pooh);
+  });
+
+  $('#feed-volibear').on('click', function(){
+    feed(volibear);
+  });
+
+  $('#steal-from-paddington').on('click', function(){
+    steal(paddington);
+  }); 
+
+  $('#steal-from-pooh').on('click', function(){
+    steal(pooh);
+  });
+
+  $('#ks-from-volibear').on('click', function(){
+    steal(volibear);
+  });
+
+});
+
+
+ ///////////////////////////////////////
+ // JOHN + HREXT05's CODE
+ ///////////////////////////////////////
+
+/*
 $(document).ready(function(){
   console.log("before\n", window.localStorage);
 
   // add event listener
   $(".add-text-btn").on("click", function(){
     $(".show-text").empty();
-    var curTextValue = $('#theKey').val(); // reading from <input>
+    var curTextValue = $("#theKey").val(); // reading from <input>
     var curKeyValue = "theKey"; // change to dynamic key?
     localStorage.setItem(curKeyValue, curTextValue);
     $(".show-text").append(curTextValue);
@@ -27,4 +186,5 @@ $(document).ready(function(){
     $(".show-text").empty();
   });
 
-});
+*/  
+;
